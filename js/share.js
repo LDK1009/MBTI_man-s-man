3c84b5289694fe35bf9b40fc51d425bbd84231a1
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2023-01-30 18:47:48 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -3,7 +3,7 @@ const url = 'https://twelvelovetype.netlify.app/';
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '십이간지 연애유형 결과';
+  const shareTitle = '상남자 MBTI 결과';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
